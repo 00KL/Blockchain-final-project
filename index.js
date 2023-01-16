b24c549f782408bf30e8c9c91c7b2ccdd5428a4a
00kl <lucas@notebook>
2023-01-16 16:48:15 -0300

    um monte de coisa... mals ai lucas do futuro

--- index.js
+++ index.js
@@ -315,10 +315,9 @@ window.onload = async () => {
 /* 2. Crião de vaga */
 const setNewVaga = () => {
 	const vaga_nome = document.querySelector("#vaga-nome").value;
-	const vaga_exigencia = [];
-	vaga_exigencia.push(document.querySelector("#vaga-exigencia").value.replace(/\s/g,'').split(","));
+	const vaga_exigencia = document.querySelector("#new-vaga-exigencia").value.replace(/\s/g,'').split(",");
 
-	Contract.createVaga(vaga_nome, vaga_exigencia[0]).then((tx) => {
+	Contract.createVaga(vaga_nome, vaga_exigencia).then((tx) => {
 		console.log(tx);
 		loadVagaName();
 	}).catch((err) => {
@@ -338,7 +337,7 @@ setNewVagaButton.addEventListener("click", setNewVaga);
 const loadVagaName = async () => {
 	console.log("loadVagaName")
 	VagaNameOptions = await Contract.getAllNomesVagas();
-      var selectBox = document.querySelector("#result-select");
+      var selectBox = document.querySelector("#vaga-name-select");
 	  removeOptions(selectBox);
       for(var i = 0, l = VagaNameOptions.length; i < l; i++){
         var codenameOption = VagaNameOptions[i];
@@ -347,3 +346,78 @@ const loadVagaName = async () => {
 }
 
 // Consulta de vagas
+const loadExigenciasByName = async () => {
+	console.log("loadExigenciasByName")
+	const selected_vaga =  document.querySelector("#vaga-name-select").value
+	const vaga_exigencias = await Contract.getVagaExigencia(selected_vaga)
+	console.log("Exigencia da vaga selecionada: ",vaga_exigencias)
+	document.querySelector("#select-vaga-exigencia").value = vaga_exigencias
+	loadCurriculo()
+}
+
+// Edita vaga
+const editExigencias = async () => {
+	console.log("editExigencias")
+	const vaga = document.querySelector("#vaga-name-select").value
+	const new_exigencias = document.querySelector("#select-vaga-exigencia").value.replace(/\s/g,'').split(",")
+	console.log(new_exigencias)
+	Contract.setVagaExigencia(vaga, new_exigencias)
+}
+
+// Criar formulario de preenchimento (currículo) 
+// baseado nas exigencias da vaga
+const loadCurriculo = async () => {
+	console.log("show, itens: ")
+	const container = document.getElementById("div_show");
+	let  fields = '';
+	const selected_vaga =  document.querySelector("#vaga-name-select").value
+	const vaga_exigencias = await Contract.getVagaExigencia(selected_vaga)
+
+	fields += '<div class="form-row">';
+	fields += '<div class="form-group col-md">';
+	fields += `<label>CPF</label>`;
+	fields += `<input class="form-control" id="cpf">`;
+	fields += '</div>';
+	fields += '</div>';
+
+	for(item of vaga_exigencias) {
+		console.log(item)
+		fields += '<div class="form-row">';
+		fields += '<div class="form-group col-md">';
+		fields += `<label>${item.charAt(0).toUpperCase() + item.slice(1)}</label>`;
+		fields += `<input class="form-control" id="exigencia_${item}">`;
+		fields += '</div>';
+		fields += '</div>';
+	}
+	container.innerHTML = fields;
+  }
+
+  // Curriculo
+  // Criar novo currículo
+  const setNewCurriculo = async () => {
+	console.log("setNewCurriculo")
+	const curriculo_vaga_name = document.querySelector("#vaga-name-select").value
+	const curriculo_cpf = document.querySelector("#cpf").value
+	const selected_vaga =  document.querySelector("#vaga-name-select").value
+	const vaga_exigencias = await Contract.getVagaExigencia(selected_vaga)
+	const curriculo_respostas = []
+	for (item of vaga_exigencias) {
+		curriculo_respostas.push(document.querySelector("#exigencia_"+item).value)
+	}
+	console.log(curriculo_respostas)
+	Contract.createVagaCurriculo(curriculo_vaga_name, curriculo_cpf, curriculo_respostas)
+  }
+
+  const loadCurriculoCPF = async () => {
+	console.log("loadCurriculoCPF")
+	const curriculo_vaga_name = document.querySelector("#vaga-name-select").value
+	console.log(curriculo_vaga_name)
+	CurriculoCPFOptions = await Contract.getVagaCPFsByName(curriculo_vaga_name)
+      var selectBox = document.querySelector("#curriculo-cpf-select");
+	  removeOptions(selectBox);
+      for(var i = 0, l = CurriculoCPFOptions.length; i < l; i++){
+        var codenameOption = CurriculoCPFOptions[i];
+        selectBox.options.add( new Option(codenameOption, codenameOption, false) );
+	}
+}
+
